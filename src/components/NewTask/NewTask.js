8114b991ba16a99f4df352ab781d211ed7afdd19
Your Name <rghzaiel@ggn-group.com>
2023-01-27 09:06:51 +0100

    create new task with custom hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,10 +1,31 @@
-import { useState } from "react";
-
+import useHttp from "../../hooks/use-http";
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const { isLoading, error, sendRequest: senTaskRequest } = useHttp();
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
+  const enterTaskHandler = (taskText) => {
+    senTaskRequest(
+      {
+        url: "https://react-http-79508-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      /*we use bind to preconfigure an object function
+      /createTask has 1 arg and we need an other one
+      /taskText should be the first argument in the function definition*/
+      createTask.bind(null, taskText)
+    );
+  };
+  /*  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const enterTaskHandler = async (taskText) => {
@@ -36,7 +57,7 @@ const NewTask = (props) => {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
-  };
+  }; */
 
   return (
     <Section>
